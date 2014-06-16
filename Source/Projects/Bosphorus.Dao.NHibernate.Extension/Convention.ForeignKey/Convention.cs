8900ce5e8e2d7987fef64cf3f4cc63f05d9073f2
Onur EKER <onur.eker@gmail.com>
2014-06-16 21:38:59 +0300

    * ForeignKey convention is added.

---
+++ Source/Projects/Bosphorus.Dao.NHibernate.Extension/Convention.ForeignKey/Convention.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentNHibernate;
+using FluentNHibernate.Conventions;
+
+namespace Bosphorus.Dao.NHibernate.Extension.Convention.ForeignKey
+{
+    public class Convention : ForeignKeyConvention
+    {
+        protected override string GetKeyName(Member property, Type type)
+        {
+            // many-to-many, one-to-many, join
+            if (property == null)
+            {
+                return type.Name + "Id";  
+            }
+
+            // many-to-one
+            return property.Name + "Id"; 
+        }
+    }
+}
