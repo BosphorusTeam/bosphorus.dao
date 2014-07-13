331ace8a9c90ac0339e828c7b27f92bda321e628
Onur EKER <onur.eker@gmail.com>
2014-07-13 15:32:29 +0300

    * Clear.bat is modified

--- Source/Clear.bat
+++ Source/Clear.bat
@@ -1,6 +1,7 @@
 @echo off
 set dir=%~dp0
 
+RD packages /S /Q
 FOR /D /R %dir% %%X IN (bin) DO RMDIR /S /Q "%%X"
 FOR /D /R %dir% %%X IN (obj) DO RMDIR /S /Q "%%X"
 FOR /D /R %dir% %%X IN (Debug) DO RMDIR /S /Q "%%X"
