84b50d36cb6cb5f774b8d78b8dd70a443d64305e
Onur EKER <onur.eker@gmail.com>
2014-11-15 03:36:41 +0200

    * AssemblyInfo.cs is modified

--- Source/Projects/Bosphorus.Dao.Lucene/Properties/AssemblyInfo.cs
+++ Source/Projects/Bosphorus.Dao.Lucene/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bosphorus.Dao.Lucene")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Bosphorus.Dao.Lucene")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Bosphorus")]
 [assembly: AssemblyProduct("Bosphorus.Dao.Lucene")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.26")]
+[assembly: AssemblyFileVersion("2.1.0.26")]
