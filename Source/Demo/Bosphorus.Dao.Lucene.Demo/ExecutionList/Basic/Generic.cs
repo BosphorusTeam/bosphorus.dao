6ee0732ee3e206fbbdc7c66617017b4222629614
Onur EKER <onur.eker@gmail.com>
2014-11-15 11:42:23 +0200

    * AssemblyInfo.cs is updated

--- Source/Demo/Bosphorus.Dao.Lucene.Demo/ExecutionList/Basic/Generic.cs
+++ Source/Demo/Bosphorus.Dao.Lucene.Demo/ExecutionList/Basic/Generic.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Bosphorus.Dao.Client.Model;
 using Bosphorus.Dao.Client.ResultTransformer;
 using Bosphorus.Dao.Core.Dao;
