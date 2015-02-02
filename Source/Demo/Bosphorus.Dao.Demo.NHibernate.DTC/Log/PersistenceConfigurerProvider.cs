458adc51a65ef20628481c661b29b682cd3328b3
Onur EKER <onur.eker@gmail.com>
2015-02-02 22:41:50 +0200

    * Unnecessary files are removed * Demo projects are fixed

--- Source/Demo/Bosphorus.Dao.Demo.NHibernate.DTC/Log/PersistenceConfigurerProvider.cs
+++ Source/Demo/Bosphorus.Dao.Demo.NHibernate.DTC/Log/PersistenceConfigurerProvider.cs
@@ -1,46 +1,23 @@
-﻿using Bosphorus.BootStapper.Common;
-using Bosphorus.Dao.NHibernate.Configuration.Fluent.PersistenceConfigurerProvider;
-using Bosphorus.Dao.NHibernate.Extension.Driver.OracleManaged;
+﻿using Bosphorus.Dao.NHibernate.Configuration.Fluent.PersistenceConfigurerProvider;
 using FluentNHibernate.Cfg.Db;
 
 namespace Bosphorus.Dao.Demo.NHibernate.DTC.Log
 {
-    public class Development : AbstractPersistenceConfigurerProvider
+    public class PersistenceConfigurerProvider : AbstractPersistenceConfigurerProvider
     {
-        private readonly Perspective perspective;
-
-        public Development(Perspective perspective)
+        public PersistenceConfigurerProvider()
             : base("LOG")
         {
-            this.perspective = perspective;
         }
 
         protected override IPersistenceConfigurer GetPersistenceProvider()
         {
-            const string dataSource = @"
-                  (DESCRIPTION =
-                    (ADDRESS = (PROTOCOL = TCP)(HOST = test-esas-2.araskargo.com.tr)(PORT = 1521))
-                    (CONNECT_DATA =
-                      (SERVER = DEDICATED)
-                      (SERVICE_NAME = esastest)
-                    )
-                  )
-            ";
-
-            string connectionString = string.Format("DATA SOURCE={0};PERSIST SECURITY INFO=True;USER ID={1};Password={2};enlist=dynamic", dataSource, "ESASLIVE", "0racle");
-
-            OracleDataClientConfiguration persistenceConfigurer = OracleDataClientConfiguration
-                .Oracle10
-                .Managed()
-                .ConnectionString(connectionString);
-
-
-            if (perspective == Perspective.Debug)
-            {
-                persistenceConfigurer = persistenceConfigurer.ShowSql().FormatSql();
-            }
-
-            return persistenceConfigurer;
+            return
+                SQLiteConfiguration
+                    .Standard
+                    .UsingFile(@".\Log.db3")
+                    .ShowSql()
+                    .FormatSql();
         }
     }
 }
